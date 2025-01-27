42b36e1ddb26193520383fd2de3014d081f2f6cf
jaexxin <jmsb02@naver.com>
2025-01-27 17:49:14 +0900

    [feat] 초기 파일 생성

---
+++ src/main/java/com/travellog/autoflow/AutoFlowApplication.java
@@ -0,0 +1,13 @@
+package com.travellog.autoflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/travellog/autoflow/AutoFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travellog.autoflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
